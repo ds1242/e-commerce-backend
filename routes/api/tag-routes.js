332fa9d2a049da08f7f6798624d60db1f4d8b94f
dshaw1242 <david.shaw1242@gmail.com>
2021-06-04 07:11:20 -0600

    started tag findOne route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,6 +18,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: { id: req.params.id },
+    include: [Product]
+  })
+  .then(dbTag => res.json(dbTag))
+  .catch(err => {
+    res.status(500).json({ message: 'Connot find a Tag with that Id'})
+  })
 });
 
 router.post('/', (req, res) => {
